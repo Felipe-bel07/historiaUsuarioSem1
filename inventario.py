3fa2aa7bb1d508e3eb5e3335a3b23f29ce0d1e00
Felipe-bel07 <andresfbeltran07@gmail.com>
2026-03-13 20:07:07 -0500

    Coding done

---
+++ inventario.py
@@ -0,0 +1,18 @@
+#This is our validation variable, which will help us to break the loop while once the user has entered the expected value
+validation = False
+
+#Here we will ask for the price and quantity of the product, but since we need to check if the value entered is valid, we will use the lop while combined with a try/except
+while validation == False:
+    try:
+        productName = input("Please enter the name of the product: ")
+        price = float(input("Please enter the price of your product: "))
+        quantity = int(input("Please enter the quantity of products you want: "))
+        validation = True 
+    except:
+        print("Please enter a valid parameter for each input.")
+
+#Here we calculate the total cost of the sell 
+totalCost = price * quantity
+
+#Print the final result to the user 
+print(f"Name of the product: {productName} | Price: {price} | Quantity: {quantity} | Total Cost: {totalCost}")
