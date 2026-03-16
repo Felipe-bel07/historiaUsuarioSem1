750153de4d77e0201cdb523e332656c51a573080
Felipe-bel07 <andresfbeltran07@gmail.com>
2026-03-16 11:36:32 -0500

    Actualziacion final

--- inventario.py
+++ inventario.py
@@ -1,10 +1,13 @@
 #This is our validation variable, which will help us to break the loop while once the user has entered the expected value
 validation = False
 
+print("Welcome to your inventory management app!")
+print(f"-" * 42)
+
 #Here we will ask for the price and quantity of the product, but since we need to check if the value entered is valid, we will use the lop while combined with a try/except
 while validation == False:
+    productName = input("Please enter the name of the product: ")
     try:
-        productName = input("Please enter the name of the product: ")
         price = float(input("Please enter the price of your product: "))
         quantity = int(input("Please enter the quantity of products you want: "))
         validation = True 
@@ -15,4 +18,6 @@ while validation == False:
 totalCost = price * quantity
 
 #Print the final result to the user 
+print(f"-" * 73)
 print(f"Name of the product: {productName} | Price: {price} | Quantity: {quantity} | Total Cost: {totalCost}")
+print(f"-" * 73)
